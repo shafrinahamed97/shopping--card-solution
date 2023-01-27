1cfcdd66c3a581e0dbb387ad5a84773666aadef4
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-01-27 16:54:00 +0600

    shopping card increase case count

---
+++ js/case.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+    const newCaseNumber = previousCaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
+
+
+})
