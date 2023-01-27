b28329c484bccd7580d9b92e2267dc9d52436adf
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-01-27 17:24:25 +0600

    shoping cart handle increase and decrease using function and update price

--- js/case.js
+++ js/case.js
@@ -1,17 +1,36 @@
-document.getElementById('btn-case-plus').addEventListener('click', function () {
+function updateCaseNumber(isCrease) {
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+    if (isCrease === true) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
     caseNumberField.value = newCaseNumber;
+
+
+    return newCaseNumber;
+
+
+}
+
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
 
 
 });
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
-    const caseNumberFiled = document.getElementById('case-number-field');
-    const caseNumberString = caseNumberFiled.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberFiled.value = newCaseNumber;
+    const newCaseNumber = updateCaseNumber(false);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
 })
